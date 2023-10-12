b4786b94edddfac1151d39181e084407c91b1f5d
Eldario85 <mielniczukdario@gmail.com>
2023-10-12 18:15:15 -0300

    first commit

---
+++ Controller/clienteController.js
@@ -0,0 +1,112 @@
+// roothpath : manejo de rutas de otros modulos del proyecto
+// express : modulo que permite gestionar y lanzar servidores
+require("rootpath")();
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var clienteDb = require("../Model/cliente");
+
+
+// -------------------------------------------------------- 
+// --rutas de escucha (endpoint) dispoibles para PERSONAS-- 
+// -------------------------------------------------------- 
+app.get('/', BuscarTodos);
+app.post('/', crear);
+app.put('/:apellido', actualizar);
+app.delete('/:id', borrar);
+app.get('/:apellido', getByApellido);
+app.get('/user/:id', getUserByCliente);
+
+
+
+// -------------------------------------------------------- 
+// ---------FUNCIONES UTILIZADAS EN ENDPOINTS ------------- 
+// --------------------------------------------------------
+
+//Listar todos los clientes
+function BuscarTodos(req, res) {
+    clienteDb.getAll(function (err, resultado) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(resultado);
+        }
+    });
+}
+
+// -------------------------------------------------------- 
+// Crear cliente
+function crear(req, res) {
+    let cliente = req.body;
+    clienteDb.create(cliente, (err, resultado) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(resultado);
+        }
+    });
+}
+
+// -------------------------------------------------------- 
+//Actualizar cliente
+function actualizar(req, res) {
+    let cliente = req.body;
+    let apellido = req.params.apellido;
+    clienteDb.update(cliente, apellido, (err, resultado) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(resultado);
+        }
+    });
+}
+
+// -------------------------------------------------------- 
+//Borrar Cliente
+function borrar(req, res) {
+    let id_persona_a_eliminar = req.params.dni;
+    clienteDb.borrar(id_persona_a_eliminar, (err, result_model) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            if (result_model.detail.affectedRows == 0) {
+                res.status(404).send(result_model.message);
+            } else {
+                res.send(result_model.message);
+            }
+        }
+    });
+}
+
+// -------------------------------------------------------- 
+//Buscar usuario por cliente
+function getUserByCliente(req, res) {
+    clienteDb.getUserByCliente(req.params.dni, (err, result_model) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(result_model);
+        }
+    });
+}
+
+// -------------------------------------------------------- 
+//Buscar por apellido
+function getByApellido(req, res) {
+    let id = req.params.apellido;
+    clienteDb.getByApellido(id, (err, result_model) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(result_model);
+        }
+    });
+}
+
+// -------------------------------------------------------- 
+
+module.exports = app;
+
+
---
+++ Model/cliente.js
@@ -0,0 +1,184 @@
+require ("rootpath") ();
+
+const mysql= require("mysql");
+const configuracion = require('../config.json');
+
+//inicializa la conexion entre el servidor y la base de datos
+var connection = mysql.createConnection(configuracion.database);
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("base de datos conectada");
+    }
+});
+
+var clienteDb = {};
+
+
+// C = CREATE
+clienteDb.create = function (datos, funCallback) {
+    consulta = "INSERT INTO cliente (id, nombre, apellido, direccion, telefono, correo_electronico, fecha_de_registro) VALUES (?,?,?,?,?,?,CURRENT_TIMESTAMP);";
+    params = [datos.id, datos.nombre, datos.apellido, datos.direccion, datos.telefono, datos.correo_electronico, datos.fecha_de_registro];
+
+    connection.query(consulta, params, (err, rows) => {
+        if (err) {
+            if (err.code == "ER_DUP_ENTRY") {
+                funCallback({
+                    message: "El cliente ya fue registrado anteriormente",
+                    detail: err
+                });
+            } else {
+                funCallback({
+                    message: "error diferente",
+                    detail: err
+                });
+            }
+        } else {
+            funCallback(undefined, {
+                message: `se creo el cliente  ${datos.nombre} ${datos.apellido}`,
+                detail: rows
+            });
+        }
+    });
+}
+
+//R = READ
+clienteDb.getAll = function (funCallback) {
+    var consulta = 'SELECT * FROM cliente';
+    connection.query(consulta, function (err, rows) {
+        if (err) {
+            funCallback({
+                message: "ha ocurrido un error inesperado al buscar el cliente",
+                detail: err
+            });
+        } else {
+            funCallback(undefined, rows);
+        }
+    });
+}
+
+
+// U = UPDATE
+// personaController --> app.put('/', actualizar);
+clienteDb.update = function (datos, apellido, funCallback) {
+    consulta = "UPDATE cliente SET nombre = ?, apellido = ?, direccion =?, telefono=?, correo_electronico=?  WHERE apellido = ?";
+    params = [datos.nombre, datos.apellido, datos.direccion, datos.telefono, datos.correo_electronico, apellido];
+
+    connection.query(consulta, params, (err, result) => {
+
+        if (err) {
+            if (err.code == "ER_DUP_ENTRY") { //dni duplicado
+                funCallback({
+                    message: "Los datos a insertar generan un cliente duplicado",
+                    detail: err
+                });
+            } else { //algun otro codigo de error
+                funCallback({
+                    message: "error diferente, analizar codigo error",
+                    detail: err
+                });
+            }
+        } else if (result.affectedRows == 0) { //persona a actualizar no encontrada
+            funCallback({
+                message: "No existe cliente que coincida con el criterio de busqueda",
+                detail: result
+            });
+        } else {
+            funcallback(undefined, {
+                message: `se modificó el cliente  ${datos.nombre} ${datos.apellido}`,
+                detail: result
+            });
+        }
+    });
+}
+
+
+
+// D = DELETE
+// personaController --> app.post('/', borrar);
+clienteDb.borrar = function (id, funCallback) {
+    consulta = "DELETE FROM persona WHERE dni = ?";
+    connection.query(consulta, id, (err, result) => {
+        if (err) {
+            funCallback({ menssage: err.code, detail: err });
+        } else {
+            if (result.affectedRows == 0) {
+                funCallback(undefined,
+                    {
+                        message: "no se encontro una persona con el dni ingresado",
+                        detail: result
+                    });
+            } else {
+                funCallback(undefined, { message: "persona eliminada", detail: result });
+            }
+        }
+    });
+}
+
+
+// personaController --> app.get('/:dni', getByDNI);
+clienteDb.getByDNI = function (dni, funCallback) {
+    connection.query('SELECT * FROM persona WHERE dni = ?', dni, (err, result) => {
+        if (err) {
+            funCallback({
+                menssage: "a ocurrido algun error inesperado al buscar la persona",
+                detail: err
+            });
+        } else if (result.length == 0) { //consulta no impacta en nada dentro de la BD
+            funCallback(undefined, {
+                menssage: `no se encontro una persona con el DNI: ${dni}`,
+                detail: result
+            });
+        } else {
+
+            funCallback(undefined, {
+                menssage: `los datos de la persona con el dni ${dni} son:`,
+                detail: result
+            });
+        }
+    });
+
+}
+
+
+// personaController --> app.get('/:persona', getUserByPersona);
+clienteDb.getUserByPersona = function (persona, funcallback) {
+
+    connection.query("select * from persona where dni = ?", persona, (err, result) => {
+        if (err) {
+            funcallback({
+                menssage: "a ocurrido algun error, posiblemente de sintaxis en buscar la persona",
+                detail: err
+            });
+        } else if (result.length == 0) { //consulta no impacta en nada dentro de la BD
+            funcallback(undefined, {
+                menssage: "no se encontro la persona buscada",
+                detail: result
+            });
+        } else {
+            consulta = "select nickname from usuario INNER JOIN persona on usuario.persona = persona.dni and usuario.persona = ?";
+            connection.query(consulta, persona, (err, result) => {
+                if (err) {
+                    funcallback({
+                        menssage: "a ocurrido algun error, posiblemente de sintaxis en buscar el nickname",
+                        detail: err
+                    });
+                } else if (result.length == 0) { //array vacio
+                    funcallback(undefined, {
+                        menssage: "la persona seleccionada no posee usuario registrado en la base de datos",
+                        detail: result
+                    });
+                } else {
+                    funcallback(undefined, { // consulta impacta bien, y el array no esta vacio 
+                        menssage: `El nikname de la persona seleccionada es ${result[0]['nickname']}`,
+                        detail: result
+                    });
+                }
+            });
+        }
+    });
+}
+
+// exportamos el objeto clienteDb para que Node.JS lo haga publico y pueda utilizarse desde otros modulos
+module.exports = clienteDb;
