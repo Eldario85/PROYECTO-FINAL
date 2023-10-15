23480ed334ff9d33e6fa594e3190149b3bfa2fa4
Eldario85 <mielniczukdario@gmail.com>
2023-10-14 23:40:54 -0300

    agregue menu, bootstrap y browser router

--- Tienda/src/App.js
+++ Tienda/src/App.js
@@ -1,23 +1,28 @@
-
-import './App.css';
-import {PrimerComponente, SegundoArticulo, TercerArticulo} from './componentes/primerComponente';
-import Titulos from './componentes/Titulos';
+import "./App.css";
+import {
+  PrimerComponente,
+  SegundoArticulo,
+  TercerArticulo,
+} from "./componentes/primerComponente";
+import Titulos from "./componentes/Titulos";
+import Menu from "./Menu";
 
 function App() {
   return (
-    
-    <div className="App bisque-background" >
-      <Titulos/>
-      <div className='contenedor-componentes '>
-        
-      <PrimerComponente />
-      <SegundoArticulo />
-      <TercerArticulo />
+    <>
+      <Menu />
 
+      <div className="App bisque-background">
+        <Titulos />
+        <div className="contenedor-componentes ">
+          <PrimerComponente />
+          <SegundoArticulo />
+          <TercerArticulo />
+        </div>
+
+        <header className="App-header bisque-background"></header>
       </div>
-
-      <header className="App-header bisque-background"></header>
-    </div>
+    </>
   );
 }
 
